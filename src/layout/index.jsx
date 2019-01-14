9f0d1b771d0a85cfbaaa631810ca258972d8a11f
JaeYeopHan <ljyhanll@gmail.com>
2019-01-14 17:52:51 +0900

    :gift: feat: Add top component

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 
+import { Top } from '../components/top'
 import { Header } from '../components/header'
 import { Footer } from '../components/footer'
 import { rhythm } from '../utils/typography'
@@ -10,17 +11,20 @@ export const Layout = ({ location, title, children }) => {
   const rootPath = `${__PATH_PREFIX__}/`
 
   return (
-    <div
-      style={{
-        marginLeft: `auto`,
-        marginRight: `auto`,
-        maxWidth: rhythm(24),
-        padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
-      }}
-    >
-      <Header title={title} location={location} rootPath={rootPath} />
-      {children}
-      <Footer />
-    </div>
+    <React.Fragment>
+      <Top />
+      <div
+        style={{
+          marginLeft: `auto`,
+          marginRight: `auto`,
+          maxWidth: rhythm(24),
+          padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
+        }}
+      >
+        <Header title={title} location={location} rootPath={rootPath} />
+        {children}
+        <Footer />
+      </div>
+    </React.Fragment>
   )
 }
