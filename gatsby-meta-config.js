cc97977cfa22f71ad19f5696881246ccf530d06e
JaeYeopHan <ljyhanll@gmail.com>
2019-01-14 21:06:10 +0900

    :gift: feat: Add bio data and styling

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,7 +1,8 @@
 module.exports = {
   title: `FELog`,
-  author: `Jbee`,
   description: `Blog posted about front end development.`,
+  author: `Jbee`,
+  introduction: `생산성.문서화.교육에 관심이 많음. 프론트 엔지니어로 커리어를 시작. 할 줄 아는 거라곤 자바스크립트 뿐. 풀스택을 하고 싶어서 이것 저것 공부중. 핀테크 분야에 있음. 가끔 Jbee라는 필명으로 글 씀. 커뮤니티에서도 볼 수 있음.`,
   siteUrl: `https://jaeyeophan.github.io`,
   social: {
     twitter: `JbeeLjyhanll`,
--- src/components/bio/index.jsx
+++ src/components/bio/index.jsx
@@ -1,29 +1,33 @@
 import React from 'react'
 import { StaticQuery, graphql } from 'gatsby'
+import Image from 'gatsby-image'
 
-import { rhythm } from '../../utils/typography'
+import './index.scss'
 
 export const Bio = () => (
   <StaticQuery
     query={bioQuery}
     render={data => {
-      const { author, social } = data.site.siteMetadata
+      const { author, social, introduction } = data.site.siteMetadata
 
       return (
-        <div
-          style={{
-            display: `flex`,
-            marginBottom: rhythm(2.5),
-          }}
-        >
-          <p>
-            Written by <strong>{author}</strong>.{` `}
-            <a href={`https://twitter.com/${social.twitter}`}>Twitter</a>
-            {` `}
+        <div className="bio">
+          <div className="author-description">
+            <Image
+              fixed={data.avatar.childImageSharp.fixed}
+              alt={author}
+              className="author-image"
+            />
+            <div>
+              <span className="author-name-prefix">Written by</span>
+              <span className="author-name">{author}</span>.
+              <div className="author-introduction">{introduction}</div>
+            </div>
+          </div>
+          <p className="author-socials">
             <a href={`https://github.com/${social.github}`}>GitHub</a>
-            {` `}
             <a href={`https://medium.com/${social.medium}`}>Medium</a>
-            {` `}
+            <a href={`https://twitter.com/${social.twitter}`}>Twitter</a>
             <a href={`https://www.facebook.com/${social.facebook}`}>Facebook</a>
           </p>
         </div>
@@ -34,9 +38,9 @@ export const Bio = () => (
 
 const bioQuery = graphql`
   query BioQuery {
-    avatar: file(absolutePath: { regex: "/profile-pic.jpg/" }) {
+    avatar: file(absolutePath: { regex: "/profile.jpeg/" }) {
       childImageSharp {
-        fixed(width: 50, height: 50) {
+        fixed(width: 72, height: 72) {
           ...GatsbyImageSharpFixed
         }
       }
@@ -44,6 +48,7 @@ const bioQuery = graphql`
     site {
       siteMetadata {
         author
+        introduction
         social {
           twitter
           github
