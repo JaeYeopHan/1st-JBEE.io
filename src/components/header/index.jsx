9d110460ad45d24db764aa0eaab19207e73a7797
JaeYeopHan <ljyhanll@gmail.com>
2019-01-17 21:02:48 +0900

    :wrench: fix: Extract style in header component

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,48 +1,15 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
-import { rhythm, scale } from '../../utils/typography'
+import './index.scss'
 
 export const Header = ({ title, location, rootPath }) => {
-  if (location.pathname === rootPath) {
-    return (
-      <h1
-        style={{
-          ...scale(1.5),
-          marginBottom: rhythm(1.5),
-          marginTop: 0,
-        }}
-      >
-        <Link
-          style={{
-            boxShadow: `none`,
-            textDecoration: `none`,
-            color: `inherit`,
-          }}
-          to={`/`}
-        >
-          {title}
-        </Link>
-      </h1>
-    )
-  }
+  const isRoot = location.pathname === rootPath
   return (
-    <h3
-      style={{
-        fontFamily: `Montserrat, sans-serif`,
-        marginTop: 0,
-      }}
-    >
-      <Link
-        style={{
-          boxShadow: `none`,
-          textDecoration: `none`,
-          color: `inherit`,
-        }}
-        to={`/`}
-      >
+    <h1 className={isRoot ? 'home-header header' : 'post-header header'}>
+      <Link to={`/`} className="link">
         {title}
       </Link>
-    </h3>
+    </h1>
   )
 }
