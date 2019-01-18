28201da19297ce1ec31f3aa5b0dff3ff70c606af
JaeYeopHan <ljyhanll@gmail.com>
2019-01-18 18:12:03 +0900

    :lipstick: styling: Styling header

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -7,7 +7,7 @@ export const Header = ({ title, location, rootPath }) => {
   const isRoot = location.pathname === rootPath
   return (
     isRoot && (
-      <h1 className="home-header header">
+      <h1 className="home-header">
         <Link to={`/`} className="link">
           {title}
         </Link>
