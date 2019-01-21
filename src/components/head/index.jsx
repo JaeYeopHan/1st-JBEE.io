de257455894f77feb3752f3fdc4c0260d544d8a1
JaeYeopHan <ljyhanll@gmail.com>
2019-01-21 18:02:19 +0900

    :gift: feat: Add viewport meta tag

--- src/components/head/index.jsx
+++ src/components/head/index.jsx
@@ -59,6 +59,11 @@ export function Head({ description, lang, meta, keywords, title }) {
                     }
                   : []
               )
+              .concat({
+                name: 'viewport',
+                content:
+                  'width=device-width, user-scalable=no, initial-scale=1, minimum-scale=1, maximum-scale=1',
+              })
               .concat(meta)}
           />
         )
