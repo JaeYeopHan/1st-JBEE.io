b2323d4fb18acb537d23479b6971e91d2b0e8d26
JaeYeopHan <ljyhanll@gmail.com>
2019-01-23 20:56:34 +0900

    :gift: feat: Add introduction data

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -2,7 +2,7 @@ module.exports = {
   title: `FELog`,
   description: `Blog posted about front end development.`,
   author: `Jbee`,
-  introduction: `생산성.문서화.교육에 관심이 많음. 프론트 엔지니어로 커리어를 시작. 할 줄 아는 거라곤 자바스크립트 뿐. 풀스택을 하고 싶어서 이것 저것 공부중. 핀테크 분야에 있음. 가끔 Jbee라는 필명으로 글 씀. 커뮤니티에서도 볼 수 있음.`,
+  introduction: `프론트 엔지니어. 생산성.문서화.클린코드.멘토링에 관심이 많음. FEConf Organizer / TypeScript Korea Organizer. 여러 개발 커뮤니티에서 볼 수 있음.`,
   siteUrl: `https://felog.netlify.com/`,
   social: {
     twitter: `JbeeLjyhanll`,
