0255eb2e7624c43af1c4d985d3cd21125cc1cb2a
JaeYeopHan <ljyhanll@gmail.com>
2019-01-23 20:32:44 +0900

    :lipstick: styling: Add style to category

--- src/components/category/index.jsx
+++ src/components/category/index.jsx
@@ -1,7 +1,35 @@
 import React from 'react'
+import randomColor from 'randomcolor'
 
 import './index.scss'
 
 export const Category = ({ category }) => {
-  return category.map((item, idx) => <div key={idx}>{item}</div>)
+  const lists = category.map((item, idx) => {
+    const color = randomColor({
+      luminosity: 'dark',
+      format: 'rgba',
+    })
+    return (
+      <li
+        key={idx}
+        className="item"
+        style={{
+          borderColor: color,
+        }}
+      >
+        <a href="" style={{ color }}>
+          {item}
+        </a>
+      </li>
+    )
+  })
+
+  return (
+    <ul className="category-container" role="tablist">
+      <li className="item">
+        <a href="">ALL</a>
+      </li>
+      {lists}
+    </ul>
+  )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,23 @@ import { Category } from '../components/category'
 import HomeContainer from '../containers/home'
 
 export default class BlogIndex extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      currentCategory: '',
+    }
+
+    this.selectCategory = this.selectCategory.bind(this)
+  }
+
+  selectCategory(value) {
+    this.setState({
+      currentCategory: value,
+    })
+  }
+
   render() {
+    const { currentCategory } = this.state
     const { data } = this.props
     const { siteMetadata } = data.site
     const { countOfInitialPost } = siteMetadata.configs
@@ -20,8 +36,12 @@ export default class BlogIndex extends Component {
       <Layout location={this.props.location} title={siteMetadata.title}>
         <Head title="FELog | Home" keywords={siteMetadata.keywords} />
         <Bio />
-        <Category category={category} />
-        <HomeContainer countOfInitialPost={countOfInitialPost} posts={posts} />
+        <Category category={category} selectCategory={this.selectCategory} />
+        <HomeContainer
+          currentCategory={currentCategory}
+          countOfInitialPost={countOfInitialPost}
+          posts={posts}
+        />
       </Layout>
     )
   }
