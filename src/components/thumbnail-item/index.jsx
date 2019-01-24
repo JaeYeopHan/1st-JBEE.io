21ed6fdd8c7d87844afc3b0306b566f82d8d5053
JaeYeopHan <ljyhanll@gmail.com>
2019-01-24 10:54:20 +0900

    :wrench: fix: Remove data of thumbnail item

--- src/components/thumbnail-item/index.jsx
+++ src/components/thumbnail-item/index.jsx
@@ -7,7 +7,6 @@ export const ThumbnailItem = ({ node }) => (
   <Link className="thumbnail" to={node.fields.slug}>
     <div key={node.fields.slug}>
       <h3>{node.frontmatter.title || node.fields.slug}</h3>
-      <small>{node.frontmatter.date}</small>
       <p dangerouslySetInnerHTML={{ __html: node.excerpt }} />
     </div>
   </Link>
