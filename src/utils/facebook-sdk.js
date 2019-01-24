1805d94e95558a977065825e67d28992c27e953a
JaeYeopHan <ljyhanll@gmail.com>
2019-01-24 13:04:39 +0900

    :wrench: fix: Extract facebook logic

---
+++ src/utils/facebook-sdk.js
@@ -0,0 +1,21 @@
+export function init(metaConfig) {
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId: metaConfig.share.facebookAppId,
+      xfbml: true,
+      version: 'v3.2',
+    })
+    FB.AppEvents.logPageView()
+  }
+  ;(function(d, s, id) {
+    var js,
+      fjs = d.getElementsByTagName(s)[0]
+    if (d.getElementById(id)) {
+      return
+    }
+    js = d.createElement(s)
+    js.id = id
+    js.src = 'https://connect.facebook.net/en_US/sdk.js'
+    fjs.parentNode.insertBefore(js, fjs)
+  })(document, 'script', 'facebook-jssdk')
+}
