ddf4a18ef98fc4ec72b5392b1be76b5565d91cc1
JaeYeopHan <ljyhanll@gmail.com>
2019-01-25 17:51:25 +0900

    :gift: feat: Add GitHub icon to top component

--- src/components/top/index.jsx
+++ src/components/top/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import { GitHubIcon } from '../social-share/github-icon'
 
 import './index.scss'
 
@@ -12,6 +13,7 @@ export const Top = ({ title, location, rootPath }) => {
           {title}
         </Link>
       )}
+      <GitHubIcon />
     </div>
   )
 }
