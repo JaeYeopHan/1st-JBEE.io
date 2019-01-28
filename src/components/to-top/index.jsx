7e3fa40e5e2aced80b2aa8faf1f2bdd58efa0c72
JaeYeopHan <ljyhanll@gmail.com>
2019-01-28 12:52:32 +0900

    :wrench: fix: Remove to-top button

--- src/components/to-top/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-import './index.scss'
-
-export const ToTop = ({ onClick }) => (
-  <button className="toTop" onClick={onClick}>
-    <span className="text">↑</span>
-  </button>
-)
