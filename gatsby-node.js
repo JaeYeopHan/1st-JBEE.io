554ad4dd2d539ecc57700fb976145b593228ba58
JaeYeopHan <ljyhanll@gmail.com>
2019-02-01 16:56:48 +0900

    :wrench: Remove about page from navigator

--- gatsby-node.js
+++ gatsby-node.js
@@ -33,25 +33,24 @@ exports.createPages = ({ graphql, actions }) => {
     }
 
     // Create blog posts pages.
-    const posts = result.data.allMarkdownRemark.edges
+    const posts = result.data.allMarkdownRemark.edges.filter(
+      ({ node }) => !!node.frontmatter.category
+    )
 
-    posts
-      .filter(({ node }) => !!node.frontmatter.category)
-      .forEach((post, index) => {
-        const previous =
-          index === posts.length - 1 ? null : posts[index + 1].node
-        const next = index === 0 ? null : posts[index - 1].node
+    posts.forEach((post, index) => {
+      const previous = index === posts.length - 1 ? null : posts[index + 1].node
+      const next = index === 0 ? null : posts[index - 1].node
 
-        createPage({
-          path: post.node.fields.slug,
-          component: blogPostTemplate,
-          context: {
-            slug: post.node.fields.slug,
-            previous,
-            next,
-          },
-        })
+      createPage({
+        path: post.node.fields.slug,
+        component: blogPostTemplate,
+        context: {
+          slug: post.node.fields.slug,
+          previous,
+          next,
+        },
       })
+    })
   })
 }
 
