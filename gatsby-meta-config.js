ffe9bfc68125baec0fe22d3c7970c4b4f9fecdea
JaeYeopHan <ljyhanll@gmail.com>
2019-02-01 16:33:14 +0900

    :wrench: fix: Change site url

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -3,7 +3,7 @@ module.exports = {
   description: `Blog posted about front end development.`,
   author: `Jbee`,
   introduction: `프론트엔드 개발자: 설계.테스트.생산성.문서화.클린코드.멘토링에 관심이 많음. FEConf Organizer / TypeScript Korea Organizer.`,
-  siteUrl: `https://felog.netlify.com/`,
+  siteUrl: `https://jbee.io`,
   social: {
     twitter: `JbeeLjyhanll`,
     github: `JaeYeopHan`,
