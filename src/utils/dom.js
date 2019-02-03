1efc65c1a697a18d303607aebe2585a9a534e209
JaeYeopHan <ljyhanll@gmail.com>
2019-02-03 18:47:22 +0900

    :gift: feat: Add util of dom

--- src/utils/dom.js
+++ src/utils/dom.js
@@ -5,6 +5,9 @@ export const getElementPosition = selector => position =>
 export const addClass = (element, className) => element.classList.add(className)
 export const removeClass = (element, className) =>
   element.classList.remove(className)
+export const hasClass = (element, className) =>
+  element.classList.contains(className)
 export const getBody = () => getElement('body')
 export const addClassToBody = className => addClass(getBody(), className)
 export const removeClassToBody = className => removeClass(getBody(), className)
+export const hasClassOfBody = className => hasClass(getBody(), className)
