9e84793a3f15758fac95f268005acc5f4c4ecb2b
JaeYeopHan <ljyhanll@gmail.com>
2019-02-07 09:54:09 +0900

    :wrench: fix: Add aria property to elements

--- src/components/social-share/github-icon/index.jsx
+++ src/components/social-share/github-icon/index.jsx
@@ -4,7 +4,11 @@ import './index.scss'
 
 export const GitHubIcon = () => {
   return (
-    <a className="github" href="https://github.com/JaeYeopHan/felog">
+    <a
+      href="https://github.com/JaeYeopHan/felog"
+      className="github"
+      aria-label="GitHub"
+    >
       <svg
         xmlns="http://www.w3.org/2000/svg"
         width="32"
