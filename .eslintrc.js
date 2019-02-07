4a5a852576d3b580c7e5f75465bc242e5e1fb2f9
JaeYeopHan <ljyhanll@gmail.com>
2019-02-07 09:11:50 +0900

    :recycle: env: Add eslint-plugin for hooks

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,20 @@
 module.exports = {
-  "env": {
-    "browser": true,
-    "es6": true,
+  env: {
+    browser: true,
+    es6: true,
   },
-  "plugins": [
-    "react",
-  ],
-  "globals": {
-    "graphql": false,
+  plugins: ['react', 'react-hooks'],
+  globals: {
+    graphql: false,
   },
-  "parserOptions": {
-    "sourceType": "module",
-    "ecmaFeatures": {
-      "experimentalObjectRestSpread": true,
-      "jsx": true,
+  parserOptions: {
+    sourceType: 'module',
+    ecmaFeatures: {
+      experimentalObjectRestSpread: true,
+      jsx: true,
     },
-  }
+  },
+  rules: {
+    'react-hooks/rules-of-hooks': 'error',
+  },
 }
