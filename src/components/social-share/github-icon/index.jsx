b2de41286fc16156ca5036bfe28ba26c2c0e0f49
JaeYeopHan <ljyhanll@gmail.com>
2019-02-11 15:08:49 +0900

    :wrench: fix: Change github link

--- src/components/social-share/github-icon/index.jsx
+++ src/components/social-share/github-icon/index.jsx
@@ -5,7 +5,7 @@ import './index.scss'
 export const GitHubIcon = () => {
   return (
     <a
-      href="https://github.com/JaeYeopHan/felog"
+      href="https://github.com/JaeYeopHan/gatsby-starter-bee"
       className="github"
       aria-label="GitHub"
     >
