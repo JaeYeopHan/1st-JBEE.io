d8f5707f7fa78bcff3df721133eaff8ce59c4262
JaeYeopHan <ljyhanll@gmail.com>
2019-02-21 16:33:16 +0900

    :gift: feat: Add error catch logic

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import * as Sentry from '@sentry/browser'
 
 import { Top } from '../components/top'
 import { Header } from '../components/header'
@@ -8,25 +9,37 @@ import { rhythm } from '../utils/typography'
 
 import './index.scss'
 
-export const Layout = ({ location, title, children }) => {
-  const rootPath = `${__PATH_PREFIX__}/`
+export class Layout extends React.Component {
+  componentDidCatch(error, errorInfo) {
+    Sentry.withScope(scope => {
+      Object.keys(errorInfo).forEach(key => {
+        scope.setExtra(key, errorInfo[key])
+      })
+      Sentry.captureException(error)
+    })
+  }
 
-  return (
-    <React.Fragment>
-      <Top title={title} location={location} rootPath={rootPath} />
-      <div
-        style={{
-          marginLeft: `auto`,
-          marginRight: `auto`,
-          maxWidth: rhythm(24),
-          padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
-        }}
-      >
-        <ThemeSwitch />
-        <Header title={title} location={location} rootPath={rootPath} />
-        {children}
-        <Footer />
-      </div>
-    </React.Fragment>
-  )
+  render() {
+    const { location, title, children } = this.props
+    const rootPath = `${__PATH_PREFIX__}/`
+
+    return (
+      <React.Fragment>
+        <Top title={title} location={location} rootPath={rootPath} />
+        <div
+          style={{
+            marginLeft: `auto`,
+            marginRight: `auto`,
+            maxWidth: rhythm(24),
+            padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
+          }}
+        >
+          <ThemeSwitch />
+          <Header title={title} location={location} rootPath={rootPath} />
+          {children}
+          <Footer />
+        </div>
+      </React.Fragment>
+    )
+  }
 }
