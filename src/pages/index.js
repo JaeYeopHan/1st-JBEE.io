bf0fdc378ff13584d2c27548b732e90ebb997967
JaeYeopHan <ljyhanll@gmail.com>
2019-06-20 11:30:49 +0900

    fix: Change scroll animation

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,7 +16,7 @@ import * as Dom from '../utils/dom'
 
 import { HOME_TITLE, CATEGORY_TYPE } from '../constants'
 
-const DEST_POS = 330
+const DEST_POS = 394
 const BASE_LINE = 80
 
 function getDistance(currentPos) {
