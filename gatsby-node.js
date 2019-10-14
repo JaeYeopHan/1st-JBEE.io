fb6763e3f49306eb50213dcb02cf170c1092f002
JaeYeopHan <ljyhanll@gmail.com>
2019-10-14 17:44:08 +0900

    env: Add redirect config

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,6 +1,8 @@
 const path = require(`path`)
 const { createFilePath } = require(`gatsby-source-filesystem`)
 
+const { redirectTable } = require('./gatsby-meta-config')
+
 exports.createPages = ({ graphql, actions }) => {
   const { createPage } = actions
 
@@ -55,7 +57,16 @@ exports.createPages = ({ graphql, actions }) => {
 }
 
 exports.onCreateNode = ({ node, actions, getNode }) => {
-  const { createNodeField } = actions
+  const { createNodeField, createRedirect } = actions
+
+  redirectTable.forEach(({ fromPath, toPath }) =>
+    createRedirect({
+      fromPath,
+      toPath,
+      isPermanent: true,
+      redirectInBrowser: true,
+    })
+  )
 
   if (node.internal.type === `MarkdownRemark`) {
     const value = createFilePath({ node, getNode })
