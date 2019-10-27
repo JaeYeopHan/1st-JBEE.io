86d027f8ff1c951aa5e324be3e4c23051bc43e7c
JaeYeopHan <ljyhanll@gmail.com>
2019-10-27 16:09:22 +0900

    fix: Change introduction in meta config

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -2,7 +2,7 @@ module.exports = {
   title: `JBEE.io`,
   description: `Blog posted about front end development.`,
   author: `Jbee`,
-  introduction: `프론트엔드 개발자: 설계.테스트.생산성.클린코드.멘토링에 관심이 많음. FEConf Organizer / TypeScript Korea Organizer.`,
+  introduction: `Web Engineer Interested in 설계.테스트.생산성.자동화.멘토링. FEConf Organizer @FEDG`,
   siteUrl: `https://jbee.io`,
   social: {
     twitter: `JbeeLjyhanll`,
