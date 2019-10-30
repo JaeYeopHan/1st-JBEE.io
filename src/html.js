7a18b6bf013dd4ae432ae75bafb035d4f09fde49
JaeYeopHan <ljyhanll@gmail.com>
2019-10-30 13:45:30 +0900

    feat: Add google adsense script

--- src/html.js
+++ src/html.js
@@ -13,6 +13,11 @@ export default class HTML extends React.Component {
             content="width=device-width, initial-scale=1, shrink-to-fit=no, minimum-scale=1, maximum-scale=2"
           />
           {this.props.headComponents}
+          <script
+            data-ad-client="ca-pub-7348753832160730"
+            async
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+          ></script>
         </head>
         <body {...this.props.bodyAttributes}>
           {this.props.preBodyComponents}
