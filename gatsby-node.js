2723e891c2ec6ac03b4ecdd445232feaec6bac89
JaeYeopHan <ljyhanll@gmail.com>
2020-01-27 23:02:49 +0900

    fix: Fix condition

--- gatsby-node.js
+++ gatsby-node.js
@@ -59,7 +59,7 @@ exports.createPages = ({ graphql, actions }) => {
 exports.onCreateNode = ({ node, actions, getNode }) => {
   const { createNodeField, createRedirect } = actions
 
-  if (!redirectTable) {
+  if (redirectTable) {
     redirectTable.forEach(({ fromPath, toPath }) =>
       createRedirect({
         fromPath,
