b2d2916771aecbe43edbd9e79b4ff4a0e763d1e6
JaeYeopHan <ljyhanll@gmail.com>
2020-04-12 15:52:26 +0900

    feat: Change style of blockquote

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -16,6 +16,7 @@ import { Utterances } from '../components/utterances'
 import * as ScrollManager from '../utils/scroll'
 
 import '../styles/code.scss'
+import '../styles/post.scss'
 
 export default ({ data, pageContext, location }) => {
   useEffect(() => {
