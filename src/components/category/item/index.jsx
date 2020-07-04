bf2a45f8ae0b83572a7ad56c95a34fd443b910e1
JaeYeopHan <ljyhanll@gmail.com>
2020-07-04 18:24:23 +0900

    feat: add scrollToCenter handler

--- src/components/category/item/index.jsx
+++ src/components/category/item/index.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, { useRef, useCallback, useEffect } from 'react'
 
-export const Item = ({ title, category, selectCategory }) => (
-  <li
-    className="item"
-    role="tab"
-    aria-selected={category === title ? 'true' : 'false'}
-  >
-    <div onClick={() => selectCategory(title)}>{title}</div>
-  </li>
-)
+export const Item = ({ title, category, selectCategory, scrollToCenter }) => {
+  const tabRef = useRef(null)
+
+  const handleClick = useCallback(() => {
+    scrollToCenter(tabRef)
+    selectCategory(title)
+  }, [])
+
+  useEffect(() => {
+    if (category === title) {
+      scrollToCenter(tabRef)
+    }
+  }, [category])
+
+  return (
+    <li
+      ref={tabRef}
+      className="item"
+      role="tab"
+      aria-selected={category === title ? 'true' : 'false'}
+    >
+      <div onClick={handleClick}>{title}</div>
+    </li>
+  )
+}
