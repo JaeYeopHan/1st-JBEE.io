1bb58c18a1f31ebc599b81cdf915e8e6bcf2cfd8
JaeYeopHan <ljyhanll@gmail.com>
2020-07-04 18:24:23 +0900

    feat: add scrollToCenter handler

--- src/components/category/index.jsx
+++ src/components/category/index.jsx
@@ -1,13 +1,25 @@
-import React from 'react'
-
-import { Item } from './item'
+import React, { useRef, useMemo } from 'react'
 import { rhythm } from '../../utils/typography'
-
 import './index.scss'
+import { Item } from './item'
 
 export const Category = ({ categories, category, selectCategory }) => {
+  const tabContainerRef = useRef(null)
+  const viewPortWidth = useMemo(() => document.documentElement.clientWidth);
+
+  const scrollToCenter = tabRef => {
+    const { offsetWidth } = tabRef.current
+    const { left } = tabRef.current.getBoundingClientRect();
+    const { scrollLeft } = tabContainerRef.current
+
+    const targetScollX = scrollLeft + left - offsetWidth / 2 - viewPortWidth / 4
+
+    tabContainerRef.current.scrollTo(targetScollX, 0)
+  }
+
   return (
     <ul
+      ref={tabContainerRef}
       className="category-container"
       role="tablist"
       id="category"
@@ -15,13 +27,14 @@ export const Category = ({ categories, category, selectCategory }) => {
         margin: `0 -${rhythm(3 / 4)}`,
       }}
     >
-      <Item title={'All'} category={category} selectCategory={selectCategory} />
+      <Item title={'All'} category={category} selectCategory={selectCategory} scrollToCenter={scrollToCenter} />
       {categories.map((item, idx) => (
         <Item
           key={idx}
           title={item}
           category={category}
           selectCategory={selectCategory}
+          scrollToCenter={scrollToCenter}
         />
       ))}
     </ul>
