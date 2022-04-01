81b6e2090c42145aaa1f8543720ac8246bf0c4f2
Jbee <ljyhanll@gmail.com>
2022-04-01 11:11:46 +0900

    Revert "fix: change profile"
    This reverts commit b1051dfa3bf7b1c15ae751d03c1870e89fc4ef99.

--- src/components/bio/index.jsx
+++ src/components/bio/index.jsx
@@ -57,7 +57,7 @@ export const Bio = () => (
 
 const bioQuery = graphql`
   query BioQuery {
-    avatar: file(absolutePath: { regex: "/profile_2.png/" }) {
+    avatar: file(absolutePath: { regex: "/profile.jpg/" }) {
       childImageSharp {
         fixed(width: 72, height: 72) {
           ...GatsbyImageSharpFixed
